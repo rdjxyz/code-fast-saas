3a86b3d2d11f00080aba61c72cc2ff70e66dbf64
rdjxyz <rick@rdj.xyz>
2025-08-31 15:58:23 -0300

    Remove duplicate ButtonLogin component from app directory

--- app/components/ButtonLogin.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-const ButtonLogin = ({ isLoggedIn, name }) => {
-  if (isLoggedIn) {
-    return <Link href='dashboard'>Welcome Back {name}</Link>;
-  }
-  return <button>Login</button>;
-};
-
-export default ButtonLogin;
